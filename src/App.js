a1ef6e169786b4ed7baf4a34d7618abff02dfd35
Lincoln Bhattachan <47911127+lincolnb17@users.noreply.github.com>
2022-07-22 15:08:41 +0545

    banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import Banner from './components/banner/Banner';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
   return(
+  <div>
     <Navbar></Navbar>
+    <Banner></Banner>
+    </div>
   )
 }
 
---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Banner.css'
+
+const Banner = () => {
+  return (
+            <div className="bimg">
+                <div className="banner">
+                <div className="container">
+                <div className="text-1">Waste Disposal management &</div> 
+                <div className="text-2"> pickup services</div>
+                <div className="text-3"> <span>"The greatest threat to our planet is the belief that someone<br></br>else will save it"</span></div>
+                <button type="button" className="btn btn-light">Request A Pickup</button>
+
+
+            </div>
+            </div>
+        </div>
+
+
+  )
+}
+
+export default Banner
