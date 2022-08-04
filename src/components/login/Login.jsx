ee30358a7a0c35fbb9dc64e8da2d212c97507d88
Lincoln Bhattachan <bhattachanl17@gmail.com>
2022-08-05 00:02:54 +0545

    logiAndSignupStateManagement

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,18 +1,37 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link } from "react-router-dom";
 import './Login.css'
+
+
+
 const Login = () => {
+
+  const [user,setUser]=useState({
+    email:"",
+    password:""
+  })
+  const handleChange = (e) => {
+    const{name,value}=e.target
+    setUser({
+      ...user,
+      [name]:value
+    })
+  
+  }
+
+
   return (
     <div className='loginp'>
+      {console.log(user)}
         <div className="bg"></div>
 
 <form>
   <div className="form-field">
-    <input type="email" placeholder="Email" required/>
+    <input type="email" name="email" value={user.email} placeholder="Email" required onChange={handleChange}/>
   </div>
   
   <div className="form-field">
-    <input type="password" placeholder="Password" required/> </div>
+    <input type="password" name="password" value={user.password}placeholder="Password" required onChange={handleChange}/> </div>
   
   <div class="form-field">
     <button className="btn" type="submit">Log in</button>
--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,21 +1,44 @@
-import React from 'react'
+import React,{useState} from 'react'
+
 import "./Signup.css"
 import { Link } from "react-router-dom";
-export const Signup = () => {
+
+const Signup = () => {
+
+  const [user,setUser]=useState({
+    email:"",
+    password:"",
+    reEnterPassword:""
+  })
+  const handleChange = (e) => {
+    const{name,value}=e.target
+    setUser({
+      ...user,
+      [name]:value
+    })
+  
+  }
+  
+
+
+
   return (
+
+    
     <div className='signupp'>
+      {console.log("User",user)}
         <div className="bg"></div>
 
 <form>
   <div className="form-field">
-    <input type="email" placeholder="Email" required/>
+    <input type="email" name="email" value={user.email} placeholder="Email" required onChange={handleChange}/>
   </div>
   
   <div className="form-field">
-    <input type="password" placeholder="Enter password" required/>
+    <input type="password" name="password" value={user.password} placeholder="Enter password" required onChange={handleChange}/>
   </div>
   <div className="form-field" id="conpass" >
-    <input type="password" placeholder="Confirm password" required/>
+    <input type="password" name="reEnterPassword" value={user.reEnterPassword} placeholder="Confirm password" required onChange={handleChange}/>
   </div>
   
   <div className="form-field">
